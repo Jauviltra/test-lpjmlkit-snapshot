f2a4d8a1c7547784753ab9b0a8fc65bd9057d026
lbm364dl <geometrydashm364@gmail.com>
2025-05-23 16:11:48 +0200

    Clean example

--- lpjmlkit.R
+++ lpjmlkit.R
@@ -1,20 +1,33 @@
-model_path <- "/home/usuario/LPJmL"
-sim_path <- "/home/usuario/LPJmL/simulation"
+# Change these paths for your computer
+model_path <- "/home/catalin/LPJmL"
+sim_path <- "/home/catalin/LPJmL/simulation"
 
+# Running only for a subset of cells, change to NA to run all cells
+# Actually NA means to use default values in `lpjml_config.cjson`
 startgrid <- 27410
 endgrid <- 27433
-use_cores <- 24
+
+# Adapt to the number of CPU cores of your computer
+# If you get an error message about cores when running, try to reduce them
+use_cores <- 4
+
+# Actual simulation year ranges (after the spinup)
 simulation_start_year <- 1901
 simulation_end_year <- 1902
 
+# Only spinup phase, no simulation
+# I wrote firstyear/lastyear for simulation_start_year because I didn't
+# know how to choose no years at all
+# Though I wrote everything in one script, spinup is meant to be run only once
 spinup_params <- tibble::tibble(
   sim_name = "spinup",
-  `startgrid` = startgrid,
-  `endgrid` = endgrid,
-  `river_routing` = FALSE,
-  `nspinup` = 2,
-  `firstyear` = simulation_start_year,
-  `lastyear` = simulation_start_year,
+  inpath = file.path(model_path, "inputs"),
+  startgrid = startgrid,
+  endgrid = endgrid,
+  river_routing = FALSE,
+  nspinup = 2,
+  firstyear = simulation_start_year,
+  lastyear = simulation_start_year,
 )
 
 spinup_config_details <- lpjmlkit::write_config(
@@ -23,16 +36,20 @@ spinup_config_details <- lpjmlkit::write_config(
   sim_path = sim_path
 )
 
+# Actual simulation scenarios after spinup. Tibble can have multiple rows,
+# one for each scenario to simulate. It uses the `-DFROM_RESTART` macro
+# to indicate that we use the already run spinup
 simulation_params <- tibble::tibble(
   sim_name = "scenario_1",
+  inpath = file.path(model_path, "inputs"),
   `-DFROM_RESTART` = TRUE,
-  `restart_filename` = "restart/spinup/restart.lpj",
-  `startgrid` = startgrid,
-  `endgrid` = endgrid,
-  `river_routing` = FALSE,
-  `nspinup` = 0,
-  `firstyear` = simulation_start_year,
-  `lastyear` = simulation_end_year,
+  restart_filename = "restart/spinup/restart.lpj",
+  startgrid = startgrid,
+  endgrid = endgrid,
+  river_routing = FALSE,
+  nspinup = 0,
+  firstyear = simulation_start_year,
+  lastyear = simulation_end_year,
 )
 
 simulation_config_details <- lpjmlkit::write_config(
@@ -41,6 +58,9 @@ simulation_config_details <- lpjmlkit::write_config(
   sim_path = sim_path
 )
 
+# Previous was just setting up configuration, now actually running the model
+
+# As mentioned before, this can be run only once
 spinup_run_details <- lpjmlkit::run_lpjml(
   spinup_config_details,
   model_path,
@@ -48,6 +68,7 @@ spinup_run_details <- lpjmlkit::run_lpjml(
   run_cmd = stringr::str_glue("mpirun -np {use_cores} ")
 )
 
+# This runs the simulations starting after the spinup
 simulation_run_details <- lpjmlkit::run_lpjml(
   simulation_config_details,
   model_path,
